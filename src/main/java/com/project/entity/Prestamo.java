631c443541e3d296193d38dbca7dcfe601fa4730
DimitriBajo <dbajobernal@gmail.com>
2026-05-29 18:22:46 +0200

    Modelo inicial de biblioteca

--- src/main/java/com/project/entity/Prestamo.java
+++ src/main/java/com/project/entity/Prestamo.java
@@ -1,5 +1,78 @@
 package com.project.entity;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class Prestamo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate fechaPrestamo;
+
+    private LocalDate fechaDevolucion;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "libro_id")
+    private Libro libro;
+
+    public Prestamo() {
+    }
+
+    public Prestamo(LocalDate fechaPrestamo,
+                    LocalDate fechaDevolucion,
+                    Usuario usuario,
+                    Libro libro) {
+
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaDevolucion;
+        this.usuario = usuario;
+        this.libro = libro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(LocalDate fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(LocalDate fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
 }
