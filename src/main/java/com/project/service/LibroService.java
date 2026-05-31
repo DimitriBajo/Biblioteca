49ec66f2dfb12e8a6eb3383849cfc9e46efd97ff
DimitriBajo <dbajobernal@gmail.com>
2026-05-31 12:56:20 +0200

    Implementada logica de prestamos

--- src/main/java/com/project/service/LibroService.java
+++ src/main/java/com/project/service/LibroService.java
@@ -1,6 +1,34 @@
 package com.project.service;
 
+import com.project.entity.Libro;
+import com.project.repository.LibroRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class LibroService {
 
+    private final LibroRepository libroRepository;
+
+    public LibroService(LibroRepository libroRepository) {
+        this.libroRepository = libroRepository;
+    }
+
+    public List<Libro> obtenerTodos() {
+        return libroRepository.findAll();
+    }
+
+    public Optional<Libro> obtenerPorId(Long id) {
+        return libroRepository.findById(id);
+    }
+
+    public Libro guardar(Libro libro) {
+        return libroRepository.save(libro);
+    }
+
+    public void eliminar(Long id) {
+        libroRepository.deleteById(id);
+    }
 }
--- src/main/java/com/project/service/PrestamoService.java
+++ src/main/java/com/project/service/PrestamoService.java
@@ -1,5 +1,46 @@
 package com.project.service;
 
+import com.project.entity.Libro;
+import com.project.entity.Prestamo;
+import com.project.repository.LibroRepository;
+import com.project.repository.PrestamoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PrestamoService {
 
+    private final PrestamoRepository prestamoRepository;
+    private final LibroRepository libroRepository;
+
+    public PrestamoService(PrestamoRepository prestamoRepository,
+                           LibroRepository libroRepository) {
+
+        this.prestamoRepository = prestamoRepository;
+        this.libroRepository = libroRepository;
+    }
+
+    public List<Prestamo> obtenerTodos() {
+        return prestamoRepository.findAll();
+    }
+
+    public Prestamo crearPrestamo(Prestamo prestamo) {
+
+        Libro libro = prestamo.getLibro();
+
+        if (!libro.getDisponible()) {
+            throw new RuntimeException("El libro ya está prestado");
+        }
+
+        libro.prestar();
+
+        libroRepository.save(libro);
+
+        return prestamoRepository.save(prestamo);
+    }
+
+    public void eliminar(Long id) {
+        prestamoRepository.deleteById(id);
+    }
 }
